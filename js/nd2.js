57128a09b49867334b5430df6089c9fdd4b8ce58
Justas0001 <justasorlakas@gmail.com>
2021-01-18 15:30:10 +0200

    reverse string

---
+++ js/nd2.js
@@ -0,0 +1,48 @@
+const a = 12;
+const b = 6;
+if (a > b) {
+console.log('Pomidoras');
+} else {
+    console.log('Bandykite kitą kartą');
+}
+
+const c = 5;
+const d = 6;
+if (c < d) {
+console.log('Pomidoras');
+} else {
+    console.log('Bandykite kitą kartą');
+}
+
+const e = 12;
+const f = 12;
+if (e === f) {
+console.log('Pomidoras');
+} else {
+    console.log('Bandykite kitą kartą');
+}
+
+const g = 12;
+const h = 12;
+if (g === h) {
+console.log('Bandykite kitą kartą');
+} else {
+    console.log('Pomidoras');
+}
+
+const i = 6;
+const j = 6;
+if (i >= j) {
+console.log('Pomidoras');
+} else {
+    console.log('Bandykite kitą kartą');
+}
+
+const k = 5;
+const l = 6;
+if (k <= l) {
+console.log('Pomidoras');
+} else {
+    console.log('Bandykite kitą kartą');
+}
+console.log('________________________________________');
